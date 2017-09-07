d933a682ff220cdddb3e00a6ce67438dda286b1e
Kai Tang <16kt16@queensu.ca>
2017-09-07 21:37:53 +0000

    adds functions exercise

---
+++ hello.js
@@ -0,0 +1,20 @@
+function sayHello() {
+
+  console.log("Hello, World");
+
+}
+
+sayHello();
+
+function sayHello1(name){
+
+  console.log("Hello, " + name);
+
+}
+
+sayHello1("Kai");
+
+sayHello1("Ming Hao");
+
+
+
