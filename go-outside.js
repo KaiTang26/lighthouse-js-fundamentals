e93110a9f42f273c5d463cef03518cf2db9f040d
Kai Tang <16kt16@queensu.ca>
2017-09-08 16:20:42 +0000

    This is practice for conditional statement

---
+++ go-outside.js
@@ -0,0 +1,84 @@
+//var raining = true;
+
+//var cold = false;
+
+var raining = false;
+
+var cold = true;
+
+if (raining){
+
+  console.log("Don't forget your umbrella! ");
+
+}
+
+if (cold){
+
+  console.log("Make sure you pick out a scarf! ")
+
+
+}
+
+console.log("Now you're ready to go outside. ")
+
+if(cold){
+
+  console.log("Make sure you pich out a scarf! ")
+
+} else {
+
+  console.log("Short sleeve are fine.")
+
+}
+
+var temperature = 50;
+
+if (temperature < 0 ){
+
+  console.log("Make sure you pick out a scarf!")
+
+} else if (temperature < 15){
+
+  console.log("Short sleeves won't cut it! ")
+
+} else {
+
+  console.log("Short sleeves are fine. ")
+
+}
+
+var isCitizen = true;
+
+var age = 26;
+
+if(isCitizen && age >=18){
+
+  console.log("You are eligible to vote. ")
+
+}
+
+if (temperature < -40 || temperature > 40){
+
+  console.log("Maybe going outside isn't such a great idea..... ")
+
+}
+
+if (!raining){
+
+  console.log("Leave your umbrella at home! ")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
