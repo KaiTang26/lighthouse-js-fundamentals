1f4b8570e3d7db78f81218aa49d8ab0e999735cf
Kai Tang <16kt16@queensu.ca>
2017-09-08 18:22:20 +0000

    while and for loop practice

---
+++ ingredients.js
@@ -0,0 +1,23 @@
+var ingrediets = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var i = 0;
+
+while ( i < ingrediets.length){
+
+  console.log(ingrediets[i]);
+
+  i++;
+
+}
+
+for (var j = 0; j < ingrediets.length; j++){
+
+  console.log(ingrediets[j]);
+
+}
+
+for (var j = ingrediets.length - 1; j >=0; j--){
+
+  console.log(ingrediets[j]);
+
+}
